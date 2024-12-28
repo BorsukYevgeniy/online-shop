c07e11cd02baa3955d2f4ab5779bf95700ef9823
Denn4ik2010 <yakimivdenis75@gmail.com>
2024-12-28 13:37:55 +0200

    Writed test to file service

--- src/files/files.service.spec.ts
+++ src/files/files.service.spec.ts
@@ -15,4 +15,25 @@ describe('FilesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create files and return file names', async () => {
+    const mockImages = [
+      {
+        fieldname: 'image',
+        originalname: 'file1.jpg',
+        encoding: '7bit',
+        mimetype: 'image/jpeg',
+        buffer: Buffer.from('mockImageData'),
+        size: 12345,
+        stream: null,
+        destination: '',
+        filename: 'file1.jpg',
+        path: '',
+      },
+    ];
+
+    const result = await service.createImages(mockImages);
+
+    expect(result).toEqual([expect.any(String)]);
+  });
 });
