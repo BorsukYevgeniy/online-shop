3bfab515c2d971e61ae5b329ba3e5f055d29c703
Denn4ik2010 <yakimivdenis75@gmail.com>
2025-07-07 09:56:55 +0300

    Refactor chat and message controllers: consolidate API and SSR controllers, update file naming conventions

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,10 +3,10 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { TokenModule } from 'src/token/token.module';
 import { ChatService } from './chat.service';
 import { ChatRepository } from './chat.repository';
-import { ChatSsrController } from './chat-ssr.controller';
+import { ChatSsrController } from './chat.ssr.controller';
 import { ChatGateway } from './chat.gateway';
 import { MessageModule } from 'src/message/message.module';
-import { ChatApiController } from './chat-api.controller';
+import { ChatApiController } from './chat.api.controller';
 
 @Module({
   imports: [TokenModule, PrismaModule, MessageModule],
--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { MessageRepository } from './message.repository';
 import { MessageService } from './message.service';
-import { MessageApiController } from './message-api.controller';
-import { ChatMessageApiController } from './chat-message-api.controller';
-import { ChatMessageSsrController } from './chat-message-ssr.controller';
+import { MessageApiController } from './message.api.controller';
+import { ChatMessageApiController } from './chat-message.api.controller';
+import { ChatMessageSsrController } from './chat-message.ssr.controller';
 import { TokenModule } from '../token/token.module';
 
 @Module({
