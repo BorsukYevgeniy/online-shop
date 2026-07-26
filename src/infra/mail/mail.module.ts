e726a9d226f9b215f175d315198ee8f263b51efc
BorsukYevgeniy <akimivdenis2@gmail.com>
2026-07-26 09:59:30 +0300

    Refactor: Remove appConfig import from MailModule

--- src/infra/mail/mail.module.ts
+++ src/infra/mail/mail.module.ts
@@ -1,13 +1,11 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigType } from '@nestjs/config';
-import appConfig from '../../config/app.config';
 import smtpConfig from '../../config/smtp.config';
 import { MailService } from './mail.service';
 
 @Module({
   imports: [
-    ConfigModule.forFeature(appConfig),
     MailerModule.forRootAsync({
       imports: [ConfigModule.forFeature(smtpConfig)],
       inject: [smtpConfig.KEY],
